3a7d5829f1f2b9151a1cfb43457c2ddd845ea5b7
BingHongLi <b97607065@gmail.com>
2021-04-27 19:03:42 +0000

    image can infer and reply

---
+++ utils/reply_send_message.py
@@ -0,0 +1,50 @@
+# 引用會用到的套件
+from linebot.models import (
+    ImagemapSendMessage,TextSendMessage,ImageSendMessage,LocationSendMessage,FlexSendMessage,VideoSendMessage,StickerSendMessage,AudioSendMessage
+)
+
+from linebot.models.template import (
+    ButtonsTemplate,CarouselTemplate,ConfirmTemplate,ImageCarouselTemplate
+    
+)
+
+from linebot.models.template import *
+
+import json
+
+def detect_json_array_to_new_message_array(fileName):
+    
+    #開啟檔案，轉成json
+    with open(fileName,encoding='utf8') as f:
+        jsonArray = json.load(f)
+    
+    # 解析json
+    returnArray = []
+    for jsonObject in jsonArray:
+
+        # 讀取其用來判斷的元件
+        message_type = jsonObject.get('type')
+        
+        # 轉換
+        if message_type == 'text':
+            returnArray.append(TextSendMessage.new_from_json_dict(jsonObject))
+        elif message_type == 'imagemap':
+            returnArray.append(ImagemapSendMessage.new_from_json_dict(jsonObject))
+        elif message_type == 'template':
+            returnArray.append(TemplateSendMessage.new_from_json_dict(jsonObject))
+        elif message_type == 'image':
+            returnArray.append(ImageSendMessage.new_from_json_dict(jsonObject))
+        elif message_type == 'sticker':
+            returnArray.append(StickerSendMessage.new_from_json_dict(jsonObject))  
+        elif message_type == 'audio':
+            returnArray.append(AudioSendMessage.new_from_json_dict(jsonObject))  
+        elif message_type == 'location':
+            returnArray.append(LocationSendMessage.new_from_json_dict(jsonObject))
+        elif message_type == 'flex':
+            returnArray.append(FlexSendMessage.new_from_json_dict(jsonObject))  
+        elif message_type == 'video':
+            returnArray.append(VideoSendMessage.new_from_json_dict(jsonObject))    
+
+
+    # 回傳
+    return returnArray
